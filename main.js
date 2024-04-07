de1cf01907060f8b21c3af791d5a9bb429bb15f0
chuaml <chuaml@outlook.com>
2024-04-07 16:23:27 +0800

    add example PWA installable app, manifest.json

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ import './style.css'
 import './src/scene-setup/init.js';
 
 
-
-
+// register service worker for PWA
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./pwa.sw.js');
+}
