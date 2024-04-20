384e79b4f37eefdf2cc2af261ea11e6533cee592
chuaml <chuaml@outlook.com>
2024-04-21 01:56:13 +0800

    remove unnecessary dev-dist files, duplicated SW

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ import './style.css'
 import './src/scene-setup/init.js';
 
 
+// vite-plugin include and uses "Workbox Build" npm module and integrated into vite build process
+// no manual config or registering Service Worker is needed
+
 // register service worker for PWA
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('pwa.sw.js');
-}
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('pwa.sw.js');
+// }
--- public/pwa.sw.js
+++ public/pwa.sw.js
@@ -1,3 +1,6 @@
+// vite-plugin include and uses "Workbox Build" npm module and integrated into vite build process
+// no manual config or registering Service Worker is needed
+
 importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
